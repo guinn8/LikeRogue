0a62e9d68bb70eac839ed812aa8464f4b3a96d3c
JohnnyMeng123 <41394774+JohnnyMeng123@users.noreply.github.com>
2018-08-05 10:43:31 -0600

    Add files via upload

--- src/actors/MyCanvas.java
+++ src/actors/MyCanvas.java
@@ -6,22 +6,22 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class MyCanvas extends Canvas {
+
 	  private GraphicsContext gc;
-	  private double xp=Core.getPlayer1().getHealth()/9;
-	  private double xe=Core.getEnemy1().getHealth()/9;
+
 
 	    public MyCanvas(int width, int height) {
 	    	super(width,height);
 	    	gc=getGraphicsContext2D();
-	    	draw(gc);
-	    
+	    	
+	        draw(gc);
 	    	
 	    }
 	    public void draw(GraphicsContext gc) {
-	    	
+	  
 	    	gc.setFill(Color.RED);
 	    	gc.setStroke(Color.RED);
-	    	gc.fillRect(0, 0, 300*xp, 10);
+	    	gc.fillRect(0, 0, Core.getPlayer1().getHealth()*30, 10);
 	    	gc.strokeRect(0, 0, 300, 10);
 	       
 	    	
@@ -29,9 +29,10 @@ public class MyCanvas extends Canvas {
 	    
 	    	gc.setFill(Color.BLUE);
 	    	gc.setStroke(Color.BLUE);
-	    	gc.fillRect(300, 0, 300*xe, 10);
+	    	gc.fillRect(300, 0, Core.getEnemy1().getHealth()*30, 10);
 	    	gc.strokeRect(300, 0, 300, 10);
-	    	gc.restore();
+	    	
+	    	
 	    	
 	    	
 	    }
