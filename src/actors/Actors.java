e8d29d40e49a6e95669e34d53c7bfbb42b221fc4
guinn8 <41174494+guinn8@users.noreply.github.com>
2018-08-12 16:09:10 -0600

    Revert "Merge branch 'master' of https://github.com/guinn8/LikeRogue"
    This reverts commit 1dd64689d12420e688dd5a09954e7a5954d99115, reversing
    changes made to ccbe49d7ec1de2cb7abad638cf9305137ab2e9de.

--- src/actors/Actors.java
+++ src/actors/Actors.java
@@ -6,15 +6,7 @@ import javafx.geometry.Bounds;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.image.ImageView;
 import main.Core;
-/**
- * This abstract class deals with the state of our player and enemy sprites. It also handles the animations
- * for the player and enemy movement.
- * 
- * @author Eric Zhang
- * @author Gavin Guinn
- * @Author Johnny Meng
- *
- */
+
 public abstract class Actors {
 	private int frames;
 	public static final int MOVERES = 1;
@@ -30,21 +22,8 @@ public abstract class Actors {
 	private int VOFF;
 	private int imageID;
 
-<<<<<<< HEAD
 	
 	Actors( int setHealth, int setDamage, int setW, int setH, int setVOFF,int setFrames){
-=======
-	/**
-	 * This is the super constructor for Actors. It is used in player and enemy when creating the characters.
-	 * @param setHealth
-	 * @param setDamage
-	 * @param setW
-	 * @param setH
-	 * @param setOFF
-	 * @param setFrames
-	 */
-	Actors( int setHealth, int setDamage, int setW, int setH, int setOFF,int setFrames){
->>>>>>> 3ee7dda9097c282de7c9eb7c03f033d532fd32e1
 		damage=setDamage;
 		health=setHealth;
 		setW(setW);
@@ -57,8 +36,7 @@ public abstract class Actors {
 	protected abstract ImageView getImageView();
 	
 	/**
-	 * This method evaluates the state of the player and enemy. It'll check their health and see if they should be dead
-	 * @return a boolean that returns true if the player/enemy is still alive and false otherwise.
+	 * 
 	 */
 	public  boolean checkAlive() {
 		if (this.getHealth() <= 0) {
@@ -68,18 +46,13 @@ public abstract class Actors {
 		return true;	
 	}
 	
-	int dir = 0;
 	/**
-	 * This method handles the animated movement of the player and enemy.
-	 * @return a integer that will determine what direction the player will be going.
+	 * @throws FileNotFoundException 
+	 *  
 	 */
-<<<<<<< HEAD
 	int dir = 0;
 	public int  move(){
 		if (deltaX==0&&deltaY==0)return dir;
-=======
-	public int move(){
->>>>>>> 3ee7dda9097c282de7c9eb7c03f033d532fd32e1
 		
 		for (int i = 0; i < 10; i++) {
 			
@@ -117,78 +90,37 @@ public abstract class Actors {
 		
 		return dir;
 	}
-	/**
-	 * A setter for delta.
-	 * @param vX
-	 * @param vY
-	 */
+
 	public void setDelta(double vX,double vY) {
 		deltaX=vX;
 		deltaY=vY;
 	}
-	
-	/**
-	 * a setter for health.
-	 * @return a integer that represents health.
-	 */
 	public int getHealth() {
 		return health;
 	}
-	
-	/**
-	 * A setter for health.
-	 * @param setHealth
-	 */
 	public void setHealth(int setHealth) {
 		
 		health=setHealth;
 	}
-	
-	/**
-	 * A getter for damage.
-	 * @return an integer that represents damage.
-	 */
 	public int getDamage() {
 		return damage;
 	}
-	
-	/**
-	 * A setter for damage.
-	 * @param setDamage
-	 */
 	public void setDamage(int setDamage) {
 		if(setDamage>=0&&setDamage<=10)damage=setDamage;
+		
 	}
-	
-	/**
-	 * A getter for deltaX (X-coordinate)
-	 * @return a double that represents deltaX/x-coordinates
-	 */
 	public double getDeltaX() {
 		return deltaX;
 	}
-	
-	/**
-	 * A getter for deltaY (Y-coordinate)
-	 * @return a double that represents deltaY/y-coordinates
-	 */
 	public double getDeltaY() {
 		return deltaY;
 	}
 	
-	/**
-	 * This method will teleport a sprite to a set of coordinates.
-	 * @param x the X-Coordinate
-	 * @param y the Y-Coordinate
-	 */
 	public void teleport(double x, double y) {
 		this.getImageView().setLayoutX(x);
 		this.getImageView().setLayoutY(y);
 	}
 	
-	/**
-	 * This method will remove a sprite from the canvas.
-	 */
 	public void remove() {
 		Core.removeSolid(this.getImageView());
 		this.getImageView().setImage(null);
@@ -198,7 +130,6 @@ public abstract class Actors {
 	}
 
 	/**
-	 * A getter for LastX
 	 * @return the lastX
 	 */
 	public double getLastX() {
@@ -206,7 +137,6 @@ public abstract class Actors {
 	}
 
 	/**
-	 * A setter for LastX
 	 * @param lastX the lastX to set
 	 */
 	public void setLastX(double lastX) {
@@ -214,7 +144,6 @@ public abstract class Actors {
 	}
 
 	/**
-	 * A getter for LastY
 	 * @return the lastY
 	 */
 	public double getLastY() {
@@ -222,36 +151,21 @@ public abstract class Actors {
 	}
 
 	/**
-	 * A setter for LastY
 	 * @param lastY the lastY to set
 	 */
 	public void setLastY(double lastY) {
 		this.lastY = lastY;
 	}
 	
-	/**
-	 * A getter for X
-	 * @return the current x-coordinate
-	 */
 	public double getX() {
 		return this.getImageView().getLayoutX();
 		
 	}
-	
-	/**
-	 * A getter for y
-	 * @return the current y-coordinate
-	 */
 	public double getY() {
 		return this.getImageView().getLayoutY();
 		
 	}
 	int animCounter=0;
-	
-	/**
-	 * This method creates the animations for player and enemy.
-	 * @param r the direction integer from move()
-	 */
 	public void animate(int r) {
 		
 		//if (this instanceof Enemy)System.out.println("counter"+animCounter*W);
--- src/main/Core.java
+++ src/main/Core.java
@@ -1,4 +1,4 @@
- package main;
+package main;
 
 import javafx.scene.layout.*;
 
@@ -39,7 +39,7 @@ public  class Core extends Application {
 	
 	private static StartMenu start= new StartMenu();
 	private static Feedbackscreen end= new Feedbackscreen();
-	public static int mapNum=0;
+	public static int mapNum;
 	private static int hitCount=0;
 	private static Pane layout = new Pane();
 	
@@ -53,7 +53,7 @@ public  class Core extends Application {
 	
 	private static File save= new File("res/save.txt");
 	
-	private static Map[] progress = new Map[4];
+	public static Map[] progress = new Map[4];
 	
 	private Image floorImage =new Image("file:res/sprites/map/floor.png");
 	private BackgroundSize backSize = new BackgroundSize(10000, 100000, true, true, true, true);
@@ -61,11 +61,7 @@ public  class Core extends Application {
 	private Background background= new Background(floor);
 	
 	private static Inventory inventory = new Inventory();
-<<<<<<< HEAD
 	private static Player player1 = new Player(10000,10);
-=======
-	private static Player player1 = new Player(10,1);
->>>>>>> 3ee7dda9097c282de7c9eb7c03f033d532fd32e1
 	
 	public static final int WIDTH=600;
 	public static final int HEIGHT=680;
@@ -77,11 +73,12 @@ public  class Core extends Application {
 
 
 	public void start(Stage stage) throws InterruptedException, FileNotFoundException {
+		
 		progress[0]= new Map(map0);
 		progress[1]= new Map(map1);
 		progress[2]= new Map(map2);
 		progress[3]= new Map(map3);
-		 
+		    
 		getLayout().setBackground(background);
 		
 		
@@ -91,18 +88,14 @@ public  class Core extends Application {
     	
 		stage.setScene(getMainScene());
 		
+		System.out.println(mapNum);
 		
-		progress[mapNum].createMap();
 		
-		getPlayer1().teleport(progress[mapNum].getPX(), progress[mapNum].getPY());
-		player1.setLastX(progress[mapNum].getPX());
-		player1.setLastY(progress[mapNum].getPY());
-		
-		getLayout().getChildren().add(solid);
 
 		stage.show();
 		
 		stage.setOnCloseRequest((WindowEvent e1)->{
+			System.out.println("ren");
 			try {
 				PrintWriter writer = new PrintWriter(getSave());
 				writer.println(mapNum);
@@ -224,28 +217,23 @@ public  class Core extends Application {
 				
 				if (object.getId().equals("chest")) {
 					solid.getChildren().remove(object);
-					int roll = (int) (Math.ceil(Math.random() * 2));
+					int roll = (int) (Math.ceil(Math.random() * 5)+1);
 					
 					if (roll== 1) {
-						if(getPlayer1().getDamage()==1){
 						inventory.setSwordVis(true);
-						getPlayer1().setDamage(3);}
-						else if(getPlayer1().getDamage()==3) {
-							inventory.setSwordVis(false);
-							inventory.setSword2Vis(true);
-							getPlayer1().setDamage(4);
-						}else if(getPlayer1().getDamage()==4) {
-							inventory.setSword2Vis(false);
-							inventory.setSword3Vis(true);
-							getPlayer1().setDamage(5);
-						}else if(getPlayer1().getDamage()==5) {
-							inventory.setSword3Vis(false);
-							inventory.setSword4Vis(true);
-							getPlayer1().setDamage(10);
-						}
+						actor.setDamage(3);
 					}
 					else if (roll == 2) {
 						inventory.setHealthVis(true);
+					}else if(roll==3) {
+						inventory.setSword2Vis(true);
+						actor.setDamage(3);
+					}else if(roll==4) {
+						inventory.setSword3Vis(true);
+						actor.setDamage(4);
+					}else if(roll==5) {
+						inventory.setSword4Vis(true);
+						actor.setDamage(5);
 					}
 					
 					return false;
@@ -302,27 +290,24 @@ public  class Core extends Application {
 	
 	public static void nextMap() {
 		
-		mapNum++;
-		if(mapNum<progress.length) {
+		System.out.println(mapNum);
+		if(mapNum<progress.length-1) {
 			try {
 				
-				progress[mapNum].createMap();
+				progress[mapNum+1].createMap();
 				getPlayer1().teleport(progress[mapNum].getPX(), progress[mapNum].getPY());
 			} catch (FileNotFoundException e) {
 	
 				e.printStackTrace();
 			}
-<<<<<<< HEAD
 			progress[mapNum].removeMap();  
 			mapNum++;
-=======
-			progress[mapNum-1].removeMap();
->>>>>>> 3ee7dda9097c282de7c9eb7c03f033d532fd32e1
 		}
 		else {
 			running=false;
 			mainScene.setRoot(endlayout);
 		}
+		
 	}
 	
 	public static void addLayout(Node n) {
@@ -393,6 +378,17 @@ public  class Core extends Application {
 	 */
 	public void setSave(File save) {
 		this.save = save;
+	}
+	
+	public static void createMap(int num) throws FileNotFoundException {
+		mapNum=num;
+		progress[num].createMap();
+		
+		getPlayer1().teleport(progress[num].getPX(), progress[num].getPY());
+		player1.setLastX(progress[num].getPX());
+		player1.setLastY(progress[num].getPY());
+		
+		getLayout().getChildren().add(solid);
 	}
 	
 }
