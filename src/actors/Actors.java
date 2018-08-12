505f5119d66d19db186d27fdd2cdb7e3a58d6df3
EZhanG14 <36180809+EZhanG14@users.noreply.github.com>
2018-08-12 14:20:43 -0600

    Add files via upload

--- src/actors/Actors.java
+++ src/actors/Actors.java
@@ -6,7 +6,15 @@ import javafx.geometry.Bounds;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.image.ImageView;
 import main.Core;
-
+/**
+ * This abstract class deals with the state of our player and enemy sprites. It also handles the animations
+ * for the player and enemy movement.
+ * 
+ * @author Eric Zhang
+ * @author Gavin Guinn
+ * @Author Johnny Meng
+ *
+ */
 public abstract class Actors {
 	private static int frames;
 	public static final int MOVERES = 1;
@@ -20,7 +28,15 @@ public abstract class Actors {
 	protected int H;
 	protected int OFF;
 
-	
+	/**
+	 * This is the super constructor for Actors. It is used in player and enemy when creating the characters.
+	 * @param setHealth
+	 * @param setDamage
+	 * @param setW
+	 * @param setH
+	 * @param setOFF
+	 * @param setFrames
+	 */
 	Actors( int setHealth, int setDamage, int setW, int setH, int setOFF,int setFrames){
 		damage=setDamage;
 		health=setHealth;
@@ -33,7 +49,8 @@ public abstract class Actors {
 	protected abstract ImageView getImageView();
 	
 	/**
-	 * 
+	 * This method evaluates the state of the player and enemy. It'll check their health and see if they should be dead
+	 * @return a boolean that returns true if the player/enemy is still alive and false otherwise.
 	 */
 	public  boolean checkAlive() {
 		if (this.getHealth() <= 0) {
@@ -43,12 +60,12 @@ public abstract class Actors {
 		return true;	
 	}
 	
-	/**
-	 * @throws FileNotFoundException 
-	 *  
-	 */
 	int dir = 0;
-	public int  move(){
+	/**
+	 * This method handles the animated movement of the player and enemy.
+	 * @return a integer that will determine what direction the player will be going.
+	 */
+	public int move(){
 		
 		for (int i = 0; i < 10; i++) {
 			//left
@@ -81,37 +98,78 @@ public abstract class Actors {
 		
 		return dir;
 	}
-
+	/**
+	 * A setter for delta.
+	 * @param vX
+	 * @param vY
+	 */
 	public void setDelta(double vX,double vY) {
 		deltaX=vX;
 		deltaY=vY;
 	}
+	
+	/**
+	 * a setter for health.
+	 * @return a integer that represents health.
+	 */
 	public int getHealth() {
 		return health;
 	}
+	
+	/**
+	 * A setter for health.
+	 * @param setHealth
+	 */
 	public void setHealth(int setHealth) {
 		
 		health=setHealth;
 	}
+	
+	/**
+	 * A getter for damage.
+	 * @return an integer that represents damage.
+	 */
 	public int getDamage() {
 		return damage;
 	}
+	
+	/**
+	 * A setter for damage.
+	 * @param setDamage
+	 */
 	public void setDamage(int setDamage) {
 		if(setDamage>=0&&setDamage<=10)damage=setDamage;
-		
 	}
+	
+	/**
+	 * A getter for deltaX (X-coordinate)
+	 * @return a double that represents deltaX/x-coordinates
+	 */
 	public double getDeltaX() {
 		return deltaX;
 	}
+	
+	/**
+	 * A getter for deltaY (Y-coordinate)
+	 * @return a double that represents deltaY/y-coordinates
+	 */
 	public double getDeltaY() {
 		return deltaY;
 	}
 	
+	/**
+	 * This method will teleport a sprite to a set of coordinates.
+	 * @param x the X-Coordinate
+	 * @param y the Y-Coordinate
+	 */
 	public void teleport(double x, double y) {
 		this.getImageView().setLayoutX(x);
 		this.getImageView().setLayoutY(y);
 	}
 	
+	/**
+	 * This method will remove a sprite from the canvas.
+	 */
 	public void remove() {
 		Core.removeSolid(this.getImageView());
 		this.getImageView().setImage(null);
@@ -121,6 +179,7 @@ public abstract class Actors {
 	}
 
 	/**
+	 * A getter for LastX
 	 * @return the lastX
 	 */
 	public double getLastX() {
@@ -128,6 +187,7 @@ public abstract class Actors {
 	}
 
 	/**
+	 * A setter for LastX
 	 * @param lastX the lastX to set
 	 */
 	public void setLastX(double lastX) {
@@ -135,6 +195,7 @@ public abstract class Actors {
 	}
 
 	/**
+	 * A getter for LastY
 	 * @return the lastY
 	 */
 	public double getLastY() {
@@ -142,21 +203,36 @@ public abstract class Actors {
 	}
 
 	/**
+	 * A setter for LastY
 	 * @param lastY the lastY to set
 	 */
 	public void setLastY(double lastY) {
 		this.lastY = lastY;
 	}
 	
+	/**
+	 * A getter for X
+	 * @return the current x-coordinate
+	 */
 	public double getX() {
 		return this.getImageView().getLayoutX();
 		
 	}
+	
+	/**
+	 * A getter for y
+	 * @return the current y-coordinate
+	 */
 	public double getY() {
 		return this.getImageView().getLayoutY();
 		
 	}
 	int animCounter=0;
+	
+	/**
+	 * This method creates the animations for player and enemy.
+	 * @param r the direction integer from move()
+	 */
 	public void animate(int r) {
 		
 		Rectangle2D anim= new Rectangle2D(W*animCounter+OFF, r*H, W, H);
--- src/actors/Enemy.java
+++ src/actors/Enemy.java
@@ -7,6 +7,13 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import main.*;
 //thanks user kemono for the enemy sprite https://opengameart.org/content/monster-sprites
+/**
+ * This class deals primarily with the enemy movement and state.
+ * @author Eric Zhang
+ * @author Gavin Guinn
+ * @Author Johnny Meng@author Eric1
+ *
+ */
 public class Enemy extends Actors {
 	
 	private Image enemySprite = new Image("file:res/sprites/enemy/skellysprite.png");
@@ -15,7 +22,13 @@ public class Enemy extends Actors {
 	private Rectangle2D enemyport= new Rectangle2D(0, 0, W, H);
 	
 
-	
+	/**
+	 * This is the constructor for enemy.
+	 * @param setX Initial x-coordinate
+	 * @param setY Initial y-coordinate
+	 * @param setHealth Initial  health
+	 * @param setDamage Initial Damage
+	 */
 	public Enemy(int setX, int setY, int setHealth, int setDamage) {
 		super(setHealth, setDamage,15,16,0, 4);
 		enemy.setScaleX(2.5);
@@ -31,6 +44,10 @@ public class Enemy extends Actors {
 	}
 
 
+	/**
+	 * This method handles movement for enemy
+	 * @return always returns 0 
+	 */
 	public int move() {
 		if (this.checkAlive()==true) {
 			double pX=Core.getPlayer1().getX();
@@ -53,20 +70,21 @@ public class Enemy extends Actors {
 
 	}
 
+	/**
+	 * Getter for Bounds
+	 * @return the current bounds of the enemy sprite
+	 */
 	@Override
 	public Bounds getBounds() {
 		return enemy.getBoundsInParent();
 	}
 
+	/**
+	 * getter for ImageView
+	 * @return an ImageView of the enemy sprite. 
+	 */
 	@Override
 	public ImageView getImageView() {
 		return enemy;
-	}
-	
-	public void setUserData(Integer num) {
-		enemy.setUserData(num);
-	}
-	public int getUserData() {
-		return (int) enemy.getUserData();
 	}
 }
