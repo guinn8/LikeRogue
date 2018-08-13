6a2d1a2eec41fd1708c71f2eb8351f1e6ebe44bd
JohnnyMeng123 <41394774+JohnnyMeng123@users.noreply.github.com>
2018-08-13 17:45:13 -0600

    Update Player.java

--- src/actors/Player.java
+++ src/actors/Player.java
@@ -18,22 +18,12 @@ import main.*;
 public class Player extends Actors {
 	
 	private Image soldierImage = new Image("file:res/sprites/player/soldier.png");
-	//private ImageView player= new ImageView(soldierImage);
-	
-	
 	private Rectangle2D soldierPort;
-	
 	private Rectangle healthBar= new Rectangle();
 	private Rectangle hbOutline= new Rectangle();
 	private ImageView damage = new ImageView();
 	private ImageView player = new ImageView(soldierImage); 
-//	private Image playerRight = new Image("file:res/sprites/player/linkRight.png");
-//	private Image playerLeft = new Image("file:res/sprites/player/linkLeft.png");
-//	private Image playerDown = new Image("file:res/sprites/player/linkDown.png");
-//	private Image playerUp = new Image("file:res/sprites/player/linkUp.png");
 	private Image damageImage = new Image("file:res/sprites/player/damage.png"); 
-	
-	
 	/**
 	 * Constructor for player
 	 * @param setHealth Initial Health
@@ -42,13 +32,6 @@ public class Player extends Actors {
 	public Player(int setHealth, int setDamage){
 		super(setHealth, setDamage,32,32,0,3);
 		soldierPort= new Rectangle2D(0, 0, getW(), getH());
-		
-		
-		/*
-		soldier.setLayoutX(200);
-		soldier.setLayoutY(150);*/
-	
-		//Core.addSolid(soldier);
 		
 		hbOutline.setX(0);
 		hbOutline.setY(660);
@@ -68,23 +51,17 @@ public class Player extends Actors {
 		damage.setFitWidth(getW());
 		damage.setImage(damageImage);
 		Core.addSolid(damage);
-		
-
-		//player.setImage(playerDown);
+	
 		player.setViewport(soldierPort);
-		
 		player.setId("player");
 		Core.addSolid(player);
-	}
-
-	
+	}	
 	/**
 	 * This method will let the player attack and shows the sprite of the attack.
 	 * @param dir the integer that determines the direction of the attack.
 	 * @return false ends the method
 	 */
-	public boolean attack(int dir) {
-		
+	public boolean attack(int dir) {	
 			//up
 			if (dir==3) {
 				damage.setLayoutX(player.getLayoutX());
@@ -106,39 +83,20 @@ public class Player extends Actors {
 				damage.setLayoutY(player.getLayoutY());
 			}
 	return false;
-	}
-	
-	
+	}	
 	/**
 	 * This method draws the health bar at the bottom
 	 */
 	public void drawHealthBar() {
 		healthBar.setWidth(this.getHealth()*60);
 	}
-
-	
 	/**
 	 * This method will teleport the damage sprite away from the canvas when its called.
 	 */
 	public void resetDamage() {
 		damage.setLayoutX(-1000);
 		damage.setLayoutY(-1000);
-	}
-	
-	/*public void setPlayerRight() {
-		//player.setImage(playerRight);
-	}
-	public void setPlayerLeft() {
-		//player.setImage(playerLeft);
-	}
-	public void setPlayerDown() {
-		//player.setImage(playerDown);
-	}
-	public void setPlayerUp() {
-		//player.setImage(playerUp);
-	}*/
-	
-	
+	}	
 	/**
 	 * Getter for bounds of the player
 	 * @return bounds the boundaries of the player sprite
@@ -154,27 +112,5 @@ public class Player extends Actors {
 	protected ImageView getImageView() {
 		return player;	
 	}
-//	int counter=0;
-//	public void animate(int r) {
-//		
-//			Rectangle2D anim= new Rectangle2D(OFF+W*counter, r*H, W, H);
-//			player.setViewport(anim);
-
-//			counter++;
-//			if (counter==3)counter=0;
-//		
-//		
-//	}
-//	
-//	public void move() {
-//		for (int i = 0; i < 10; i++) {
-//			if (Core.check(this)) {
-//				
-//				
-//				
-//			}
-//		}
-//		setDelta(0,0);
-//	}
-	}
+}
 
