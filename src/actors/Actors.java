62a7d1ab8936a557db8758b20f8aa78428f9384c
EZhanG14 <36180809+EZhanG14@users.noreply.github.com>
2018-08-13 16:40:02 -0600

    Update Actors.java

--- src/actors/Actors.java
+++ src/actors/Actors.java
@@ -6,11 +6,26 @@ import javafx.geometry.Bounds;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.image.ImageView;
 import main.Core;
+
+/**
+ * This interface has methods that affect the state of the player and/or enemy in some way.
+ *
+ */
 interface GameplayInterFace{
 	public abstract boolean attack(int dir);
 	public abstract void drawHealthBar();
 	public abstract  void resetDamage(); 
 }
+
+/**
+ * This is the super constructor for Actors. It is used in player and enemy when creating the characters.
+ * @param setHealth
+ * @param setDamage
+ * @param setW
+ * @param setH
+ * @param setOFF
+ * @param setFrames
+ */
 public abstract class Actors implements GameplayInterFace {
 	private  int frames;
 
@@ -27,7 +42,15 @@ public abstract class Actors implements GameplayInterFace {
 
 
 
-	
+	/**
+	 * This is the super constructor for Actors. It is used in player and enemy when creating the characters.
+	 * @param setHealth
+	 * @param setDamage
+	 * @param setW
+	 * @param setH
+	 * @param setOFF
+	 * @param setFrames
+	 */
 	Actors( int setHealth, int setDamage, int setW, int setH, int setVOFF,int setFrames){
 		damage=setDamage;
 		health=setHealth;
@@ -36,12 +59,22 @@ public abstract class Actors implements GameplayInterFace {
 		frames=setFrames;
 
 	}
-
+	
+	/**
+	 * abstract method for getting the boundaries of an object
+	 * @return the boundaries of an object
+	 */
 	public abstract Bounds getBounds();
+	
+	/**
+	 * abstract method for getting the ImageView of an object.
+	 * @return the ImageView.
+	 */
 	protected abstract ImageView getImageView();
 	
 	/**
-	 * 
+	 * This method evaluates the state of the player and enemy. It'll check their health and see if they should be dead
+	 * @return a boolean that returns true if the player/enemy is still alive and false otherwise.
 	 */
 	public  boolean checkAlive() {
 		if (this.getHealth() <= 0) {
@@ -56,6 +89,11 @@ public abstract class Actors implements GameplayInterFace {
 	 *  
 	 */
 	public int dir = 0;
+	
+	/**
+	 * This method handles the animated movement of the player and enemy.
+	 * @return a integer that will determine what direction the player will be going.
+	 */
 	public int  move(){
 		if (deltaX==0&&deltaY==0)return dir;
 		
@@ -95,37 +133,80 @@ public abstract class Actors implements GameplayInterFace {
 		
 		return dir;
 	}
-
+	
+	/**
+	 * A setter for delta.
+	 * @param vX
+	 * @param vY
+	 */
 	public void setDelta(double vX,double vY) {
 		deltaX=vX;
 		deltaY=vY;
 	}
+	
+	/**
+	 * a getter for health.
+	 * @return a integer that represents health.
+	 */
 	public int getHealth() {
 		return health;
 	}
+	
+	/**
+	 * A setter for health.
+	 * @param setHealth
+	 */
 	public void setHealth(int setHealth) {
 		
 		health=setHealth;
 	}
+	
+	/**
+	 * A getter for damage.
+	 * @return an integer that represents damage.
+	 */
 	public int getDamage() {
 		return damage;
 	}
+	
+	/**
+	 * A setter for damage.
+	 * @param setDamage
+	 */
 	public void setDamage(int setDamage) {
 		if(setDamage>=0&&setDamage<=10)damage=setDamage;
 		
 	}
+	
+	/**
+	 * A getter for deltaX (X-coordinate)
+	 * @return a double that represents deltaX/x-coordinates
+	 */
 	public double getDeltaX() {
 		return deltaX;
 	}
+	
+	/**
+	 * A getter for deltaY (Y-coordinate)
+	 * @return a double that represents deltaY/y-coordinates
+	 */
 	public double getDeltaY() {
 		return deltaY;
 	}
 	
+	/**
+	 * This method will teleport a sprite to a set of coordinates.
+	 * @param x the X-Coordinate
+	 * @param y the Y-Coordinate
+	 */
 	public void teleport(double x, double y) {
 		this.getImageView().setLayoutX(x);
 		this.getImageView().setLayoutY(y);
 	}
 	
+	/**
+	 * This method will remove a sprite from the canvas.
+	 */
 	public void remove() {
 		Core.removeSolid(this.getImageView());
 		this.getImageView().setImage(null);
@@ -135,6 +216,7 @@ public abstract class Actors implements GameplayInterFace {
 	}
 
 	/**
+	 * A getter for LastX
 	 * @return the lastX
 	 */
 	public double getLastX() {
@@ -142,6 +224,7 @@ public abstract class Actors implements GameplayInterFace {
 	}
 
 	/**
+	 * A setter for LastX
 	 * @param lastX the lastX to set
 	 */
 	public void setLastX(double lastX) {
@@ -149,6 +232,7 @@ public abstract class Actors implements GameplayInterFace {
 	}
 
 	/**
+	 * A getter for LastY
 	 * @return the lastY
 	 */
 	public double getLastY() {
@@ -156,21 +240,36 @@ public abstract class Actors implements GameplayInterFace {
 	}
 
 	/**
+	 * A setter for LastY
 	 * @param lastY the lastY to set
 	 */
 	public void setLastY(double lastY) {
 		this.lastY = lastY;
 	}
 	
+	/**
+	 * A getter for X
+	 * @return the current x-coordinate
+	 */
 	public double getX() {
 		return this.getImageView().getLayoutX();
 		
 	}
+	
+	/**
+	 * A getter for y
+	 * @return the current y-coordinate
+	 */
 	public double getY() {
 		return this.getImageView().getLayoutY();
 		
 	}
 	int animCounter=0;
+	
+	/**
+	 * This method creates the animations for player and enemy.
+	 * @param r the direction integer from move()
+	 */
 	public void animate(int r) {
 		
 		//if (this instanceof Enemy)System.out.println("counter"+animCounter*W);
@@ -189,6 +288,7 @@ public abstract class Actors implements GameplayInterFace {
 	}
 
 	/**
+	 * Getter for w
 	 * @return the w
 	 */
 	public int getW() {
@@ -196,6 +296,7 @@ public abstract class Actors implements GameplayInterFace {
 	}
 
 	/**
+	 * Setter for w
 	 * @param w the w to set
 	 */
 	public void setW(int w) {
@@ -203,6 +304,7 @@ public abstract class Actors implements GameplayInterFace {
 	}
 
 	/**
+	 * Getter for h
 	 * @return the h
 	 */
 	public int getH() {
@@ -210,6 +312,7 @@ public abstract class Actors implements GameplayInterFace {
 	}
 
 	/**
+	 * Setter for h
 	 * @param h the h to set
 	 */
 	public void setH(int h) {
