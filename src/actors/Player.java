1c642a8b3a25e6dce7d98235e4695143d9ba9040
JohnnyMeng123 <41394774+JohnnyMeng123@users.noreply.github.com>
2018-08-14 14:31:35 -0600

    Update Player.java

--- src/actors/Player.java
+++ src/actors/Player.java
@@ -8,7 +8,6 @@ import main.*;
 //thanks user https://opengameart.org/users/erbarlow for player sprites
 
 
-
 /**
  * This class primarily deals with player movement,state and actions.
  * @author Eric Zhang
@@ -17,21 +16,12 @@ import main.*;
  *thanks user https://opengameart.org/users/erbarlow for player sprites
  */
 public class Player extends Actors {
-	
-
-	//private ImageView player= new ImageView(soldierImage);
-	
-	
 	private Rectangle2D soldierPort;
-	
-	
 	private ImageView damage = new ImageView(new Image("file:res/sprites/player/damage.png"));
 	private ImageView player = new ImageView(new Image("file:res/sprites/player/soldier.png")); 
-
-	
-	
 	private boolean attacking=false;
 
+	
 	/**
 	 * Constructor for player
 	 * @param setHealth Initial Health
@@ -41,36 +31,24 @@ public class Player extends Actors {
 		super(setHealth, setDamage,32,32,0,3);
 		soldierPort= new Rectangle2D(0, 0, getW(), getH());
 		
-		
-		/*
-		soldier.setLayoutX(200);
-		soldier.setLayoutY(150);*/
-	
-		//Core.addSolid(soldier);
-
-		
 		damage.setId("damage");
 		damage.setFitHeight(getH());
 		damage.setFitWidth(getW());
-	
-		Core.addSolid(damage);
 		
-
-		//player.setImage(playerDown);
-		player.setViewport(soldierPort);
-		
+		player.setViewport(soldierPort);		
 		player.setId("player");
 		Core.addSolid(player);
+		Core.addSolid(damage);
 	}
 
+	
 	/**
 	 * This method will let the player attack and shows the sprite of the attack.
 	 * @param dir the integer that determines the direction of the attack.
 	 * @return false ends the method
 	 * 
 	 */
-	public boolean attack() {
-		
+	public boolean attack() {	
 			//up
 			if (getDirection()==3) {
 				damage.setLayoutX(player.getLayoutX());
@@ -91,7 +69,7 @@ public class Player extends Actors {
 				damage.setLayoutX(player.getLayoutX()+getW()+2);
 				damage.setLayoutY(player.getLayoutY());
 			}
-	return false;
+	        return false;
 	}
 	
 
@@ -103,9 +81,15 @@ public class Player extends Actors {
 		damage.setLayoutY(-1000);
 	}
 	
+	
+	/**
+	 * This method will get Bounds
+	 * @return getBoundsInParent()
+	 */
 	public Bounds getBounds() {
 		return player.getBoundsInParent();
 	}
+	
 	
 	/**
 	 * Getter for ImageView of the player
@@ -115,22 +99,21 @@ public class Player extends Actors {
 		return player;	
 	}
 
-	
 
-	/**
+	/**getmethod
 	 * @return the attacking
 	 */
 	public boolean isAttacking() {
 		return attacking;
 	}
 
-	/**
+	
+	/**setmethod
 	 * @param attacking the attacking to set
 	 */
 	public void setAttacking(boolean attacking) {
 		this.attacking = attacking;
 	}
+}
 
-
-	}
 
